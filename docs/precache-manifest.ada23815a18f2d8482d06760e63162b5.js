abea3345d802b0f93cc8cbc9492886f26b52340d
GeorgeSteel <julianacero98@gmail.com>
2020-06-28 21:48:37 -0500

    docs added

---
+++ docs/precache-manifest.ada23815a18f2d8482d06760e63162b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211d2d504376f70a99db96177f67897f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e0320dd621aac625795",
+    "url": "/static/css/main.af3bfa24.chunk.css"
+  },
+  {
+    "revision": "bbd14bb20d73ad5f6f29",
+    "url": "/static/js/2.5ddef163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ddef163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0320dd621aac625795",
+    "url": "/static/js/main.37cd9ae5.chunk.js"
+  },
+  {
+    "revision": "e775e7a58d8a9a5e5140",
+    "url": "/static/js/runtime-main.d76e4de2.js"
+  }
+]);
